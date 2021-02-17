6079ffc09a77c9759cac5420a1981f264b979a38
Aicos <richard.sw268@gmail.com>
2021-02-17 06:39:22 -0500

    handleAdd

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,28 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Formulario = () => {
-    return (
-        <>
-            <h2>Desde formulario</h2>
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
 
-            <form>
-                <label htmlFor="nombremascota">Nombre de la mascota</label>
-                <input type="text" id="nombremascota" name="mascota" className="u-full-width" placeholder="Nombre de tu mascota"/>
+  // Función que se ejecuta cada que el usuario escribe en un input
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-                <label htmlFor="nombredueño">Nombre del dueño</label>
-                <input type="text" id="nombredueño" name="propietario" className="u-full-width" placeholder="Nombre del dueño"/>
+  //Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+  return (
+    <>
+      <h2>Crear cita</h2>
+      <form>
+        <label htmlFor="nombremascota">Nombre de la mascota</label>
+        <input
+          type="text"
+          id="nombremascota"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre de tu mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
 
-                <label htmlFor="fecha">Nombre del dueño</label>
-                <input type="date" id="fecha" name="fecha" className="u-full-width"/>
+        <label htmlFor="nombredueño">Nombre del dueño</label>
+        <input
+          type="text"
+          id="nombredueño"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre del dueño"
+          onChange={actualizarState}
+          value={propietario}
+        />
 
-                <label htmlFor="time">Hora</label>
-                <input type="time" id="time" name="time" className="u-full-width"/>
+        <label htmlFor="fecha">Nombre del dueño</label>
+        <input
+          type="date"
+          id="fecha"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
 
-                <label htmlFor="sintomas">Síntomas</label>
-                <textarea name="sintomas" id="sintomas" cols="30" rows="10" className="u-full-width"></textarea>
+        <label htmlFor="time">Hora</label>
+        <input
+          type="time"
+          id="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
 
-                <button type="submit" className="u-full-width button-primary">Reservar cita</button>
-            </form>
-        </>
-    )
-}
+        <label htmlFor="sintomas">Síntomas</label>
+        <textarea
+          name="sintomas"
+          id="sintomas"
+          cols="30"
+          rows="10"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Reservar cita
+        </button>
+      </form>
+    </>
+  );
+};
